ef9e647a89a5f91cf9d5ad5b47c173352fb29599
Anasurim <108210477+Anasurim@users.noreply.github.com>
2023-03-07 12:31:56 +0000

    Deploying to gh-pages from @ Anasurim/goit-js-hw-09@ab5478f1528f34dd5c4de101b9909b3c7fcd8bfd 🚀

---
+++ 01-color-switcher.37ed092f.js
@@ -0,0 +1,2 @@
+!function(){var t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]")},n=null;t.start.addEventListener("click",(function(){n||(n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3));t.start.disabled=!0})),t.stop.addEventListener("click",(function(){clearInterval(n),t.start.disabled=!1,n=null}))}();
+//# sourceMappingURL=01-color-switcher.37ed092f.js.map
