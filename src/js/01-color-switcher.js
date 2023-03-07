ab5478f1528f34dd5c4de101b9909b3c7fcd8bfd
Lebedynskyi Roman <cliffcrystall@gmail.com>
2023-03-07 13:31:26 +0100

    homework 9: color-switcher completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
 
+let intervalId = null;
+
+refs.start.addEventListener('click', changeBodyTheme);
+refs.stop.addEventListener('click', stopChangeBodyTheme);
+
+function changeBodyTheme() {
+  if (!intervalId) {
+    intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+  refs.start.disabled = true;
+}
+
+function stopChangeBodyTheme() {
+  clearInterval(intervalId);
+  refs.start.disabled = false;
+  // release our intervalID from the variable
+  intervalId = null;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
