55667caf7e54fc285ad01f898f854e32170e87de
Lebedynskyi Roman <cliffcrystall@gmail.com>
2023-03-11 00:59:30 +0100

    homework 9: task2: validDate check completed

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,13 +9,24 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
-    console.log(new Date());
 
-    if (selectedDates[0] <= new Date()) {
-      return Notify.failure('Please choose a date in the future');
-    }
-    return Notify.success('Selected date is correct');
+    isDateValid(selectedDates);
   },
 };
 
 flatpickr("input[type = 'text']", options);
+
+const refs = {
+  button: document.querySelector('button'),
+};
+
+refs.button.disabled = true;
+
+function isDateValid(selectedDates) {
+  if (selectedDates[0] <= new Date()) {
+    return Notify.failure('Please choose a date in the future');
+  } else {
+    refs.button.disabled = false;
+    return Notify.success('Selected date is correct');
+  }
+}
