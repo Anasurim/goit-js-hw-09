88c4c87af788742adb5062894bf3ac5b0965a831
Lebedynskyi Roman <cliffcrystall@gmail.com>
2023-03-12 19:03:36 +0100

    homework 9: task3: completed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  form: document.querySelector('.form'),
+  inputDelay: document.querySelector('input[name="delay"]'),
+  inputStep: document.querySelector('input[name="step"]'),
+  inputAmount: document.querySelector('input[name="amount"]'),
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      }
+      reject({ position, delay });
+    }, delay);
+  });
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const amount = parseInt(refs.inputAmount.value);
+  const delay = parseInt(refs.inputDelay.value);
+  const step = parseInt(refs.inputStep.value);
+
+  for (let index = 0; index < amount; index += 1) {
+    createPromise(index, delay + index * step)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
+
+refs.form.addEventListener('submit', onFormSubmit);
